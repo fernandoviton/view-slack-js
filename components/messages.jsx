fd7e81562f72f02c3680726cde45eac6ad57fce5
Fernndo Viton <fcode@viton.org>
2016-12-23 20:14:47 -0800

    add load more buttons, cleanup, doc cleanup

--- components/messages.jsx
+++ components/messages.jsx
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import MessageGroups from './messageGroups'
+import loadMessages from '../middleware/loadMessages'
+import {findIndexReverse} from '../util/array'
 
 const htmlItemFromMessage = (message) => {
   return <li key={message.id}>
@@ -14,22 +16,45 @@ const htmlItemsFromMessageGroup = (messageGroup) => {
 }
 
 const htmlMessageGroupHeader = (date) => {
-  return <h><font color="red">{date}</font></h>
+  return <p><font color="red">{date}</font></p>
+}
+
+const htmlLoadMoreButton = (label, onClick) => {
+  return <button onClick={onClick}>{label}</button>
 }
 
 export default class Messages extends Component {
 	render() {
     const { store } = this.context;
     const { items } = store.getState().messageGroups;
+
+    const indexOfFirstLoadedMessageGroup = items.findIndex((messageGroup) => {
+      return messageGroup.messages.items.length > 0
+    })
+    const indexOfLastLoadedMessageGroup = findIndexReverse(items, (messageGroup) => {
+      return messageGroup.messages.items.length > 0
+    })
+    const htmlShowEarlierButton = indexOfFirstLoadedMessageGroup <= 0
+      ? []
+      : htmlLoadMoreButton('load previous conversation', () => {
+        loadMessages(store, items[indexOfFirstLoadedMessageGroup-1].name)
+      })
+    const htmlShowLaterButton = indexOfLastLoadedMessageGroup >= items.length - 1
+      ? []
+      : htmlLoadMoreButton('load next conversation', () => {
+        loadMessages(store, items[indexOfLastLoadedMessageGroup+1].name)
+      })
+
     return (
       <div>
-        <MessageGroups/>
         <ul>
+          {htmlShowEarlierButton}
           {items.map((messageGroup) => {
             return messageGroup.messages.items.length == 0 
               ? []
               : [htmlMessageGroupHeader(messageGroup.name), htmlItemsFromMessageGroup(messageGroup)]
           })}
+          {htmlShowLaterButton}
         </ul>
       </div>
       );
--- docs/state.md
+++ docs/state.md
@@ -1,3 +1,5 @@
+### State description
+
 state
 - channels
 	- isLoading
@@ -13,33 +15,25 @@ state
 		}
 	}
 
--- message:
-	{
-		id
-		text
-	}
+message
+	- id
+	- text
 
-Loading messages:
-1. Adding more messages consists of:
-	a. change state to start loading for the ones in progress
-	b. trigger asyncs to get data
-	c. change state to finish loading with data.  This will create new array of messageGroups.items 
-	with the affected items udpate
-1.5.  Changing messageGroups consists of:
-	a. clearing active messages
-2. To display messages:
+### Messages details:
+1. Adding more messages to a loaded message group consists of:
+	a. dispatch a startLoadMessages to start loading for the messageGroup (messageGroups.items) in progress
+	b. fetch the data (likely through an async)
+	c. dispatch a finishedLoadMessages to push the changed data into the state
+2. To render messages
 	a. display each message group in order (with headers as desired)
 	b. display a "load more" or "loading" ui element before and/or after the messages 
 3. Data structure needed for this:
 	a. ordered (by date - which is name) list of message groups
-	b. linear speed access to message groups on write ok
-4. Actions for this:
-	a. [start|finished]LoadMessages
+	b. linear speed access to message groups on write is good enough (for read)
 
-data flow
+### Basic Flow
 1. load channels
 2. set active channel
 3. load message groups
-4. set active message group
-5. load messages
-6. set messages
+4. load messages
+5. set messages
