f9e2b1d381112aa2d8dc79e39fadd74991237da5
Fernndo Viton <fcode@viton.org>
2017-06-26 21:05:33 -0700

    fixup some eslint

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ import { app, BrowserWindow } from 'electron';
 let mainWindow = null;
 
 app.on('window-all-closed', () => {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({width: 800, height: 600, globals: {id: 17}});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+	mainWindow = new BrowserWindow({ width: 800, height: 600, globals: { id: 17 } });
+	mainWindow.loadURL(`file://${__dirname}/index.html`);
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
 });
